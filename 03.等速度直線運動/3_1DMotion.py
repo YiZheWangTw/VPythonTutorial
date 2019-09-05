c48518fc9191ee292df763f54129d4d53ab99ce7
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-05 21:22:17 +0800

    Update 3_1DMotion.py

--- 03.等速度直線運動/3_1DMotion.py
+++ 03.等速度直線運動/3_1DMotion.py
@@ -1,6 +1,7 @@
 """
  VPython教學: 3.等速度直線運動
- 日期: 2018/2/18
+ Ver. 1: 2018/2/18
+ Ver. 2: 2019/9/5
  作者: 王一哲
 """
 from vpython import *
@@ -16,26 +17,20 @@ dt = 0.01    # 時間間隔
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 box 物件產生木塊及地板 http://www.glowscript.org/docs/VPythonDocs/box.html
-    (3) 顏色選項說明 http://www.glowscript.org/docs/VPythonDocs/color.html
-    (4) 用 graph 產生繪圖視窗 http://www.glowscript.org/docs/VPythonDocs/graph.html
 """
-scene = canvas(title = "1D Motion", width = 800, height = 600, x = 0, y = 0, center = vector(0, 0.1, 0),
-               background = vector(0, 0.6, 0.6))
-floor = box(pos = vector(0, 0, 0), length = L, height = size*0.1, width = L*0.5, color = color.blue)
-#floor = box(pos = vec(0, 0, 0), size = vec(L, size*0.1, L*0.5), color = color.blue)
-cube = box(pos = vector(-L*0.5 + size*0.5, size*0.55, 0), length = size, height = size, width = size, color = color.red)
-cube.v = vector(v, 0, 0)
-gd = graph(title = "x-t plot", width = 600, height = 450, x = 0, y = 600, xtitle = "t(s)", ytitle = "x(m)")
-gd2 = graph(title = "v-t plot", width = 600, height = 450, x = 0, y = 1050, xtitle = "t(s)", ytitle = "v(m/s)")
-xt = gcurve(graph = gd, color = color.red)
-vt = gcurve(graph = gd2, color = color.red)
+scene = canvas(title="1D Motion", width=800, height=600, x=0, y=0, center=vec(0, 0.1, 0), background=vec(0, 0.6, 0.6))
+floor = box(pos=vec(0, 0, 0), size=vec(L, 0.1*size, 0.5*L), color=color.blue)
+cube = box(pos=vec(-0.5*L + 0.5*size, 0.55*size, 0), size=vec(size, size, size), color=color.red, v=vec(v, 0, 0))
+#cube = box(pos=vec(-0.5*L + 0.5*size, 0.55*size, 0), length=size, height=size, width=size, color=color.red, v=vec(v, 0, 0))
+gd = graph(title="x-t plot", width=600, height=450, x=0, y=600, xtitle="t(s)", ytitle="x(m)")
+gd2 = graph(title="v-t plot", width=600, height=450, x=0, y=1050, xtitle="t(s)", ytitle="v(m/s)")
+xt = gcurve(graph=gd, color=color.red)
+vt = gcurve(graph=gd2, color=color.red)
 
 """
  3. 物體運動部分, 木塊到達地板邊緣時停止執行
 """
-while(cube.pos.x <= L*0.5 - size*0.5):
+while(cube.pos.x <= 0.5*L- 0.5*size):
     rate(1000)
     cube.pos.x += v*dt
     xt.plot(pos = (t, cube.pos.x))
