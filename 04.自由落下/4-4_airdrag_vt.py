93017529e3905d27e83ec9fb1821fe5108b0d5ec
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-06 11:44:59 +0800

    Update 4-4_airdrag_vt.py

--- 04.自由落下/4-4_airdrag_vt.py
+++ 04.自由落下/4-4_airdrag_vt.py
@@ -1,6 +1,8 @@
 """
  VPython教學: 4-4.自由落下, 有空氣阻力, 求終端速度
- 日期: 2018/3/8
+ Ver. 1: 2018/3/8
+ Ver. 2: 2019/2/2
+ Ver. 3: 2019/9/6
  作者: 王一哲
 """
 from vpython import *
@@ -8,60 +10,68 @@ from vpython import *
 """
  1. 參數設定, 設定變數及初始值
 """
-size = 1     # 小球半徑
-m = 1        # 小球質量
-h = 0        # 小球離地高度
-g = 9.8      # 重力加速度 9.8 m/s^2
-b = 0.3      # 空氣阻力 f = -bv
-t = 0        # 時間
-dt = 0.001   # 時間間隔
+size=1     # 小球半徑
+m=1        # 小球質量
+h=0        # 小球離地高度
+g=9.8      # 重力加速度 9.8 m/s^2
+b=0.1      # 空氣阻力 f=-bv
+c1, c2=color.red, color.green
+t=0        # 時間
+dt=0.0001  # 時間間隔
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 box 物件產生地板 http://www.glowscript.org/docs/VPythonDocs/box.html
-    (3) 用 sphere 物件產生小球 http://www.glowscript.org/docs/VPythonDocs/sphere.html
-    (4) 用 graph 產生繪圖視窗 http://www.glowscript.org/docs/VPythonDocs/graph.html
 """
-scene = canvas(title = "Free Fall", width = 600, height = 600, x = 0, y = 0, center = vector(0, h/2, 0),
-               background = vector(0, 0.6, 0.6))
-ball = sphere(pos = vector(0, h, 0), radius = size, color = color.red)
-ball.v = vector(0, 0, 0)
-ball.a = vector(0, -g, 0)
-gd = graph(title = "y-t plot", width = 600, height = 450, x = 0, y = 600, xtitle = "t(s)", ytitle = "y(m)")
-gd2 = graph(title = "v-t plot", width = 600, height = 450, x = 0, y = 1050, xtitle = "t(s)", ytitle = "v(m/s)")
-gd3 = graph(title = "a-t plot", width = 600, height = 450, x = 0, y = 1500, xtitle = "t(s)", ytitle = "a(m/s^2)")
-yt = gcurve(graph = gd, color = color.red)
-vt = gcurve(graph = gd2, color = color.red)
-at = gcurve(graph = gd3, color = color.red)
+scene = canvas(title="Free Fall", width=400, height=400, x=0, y=0, center=vec(0, h/2, 0), background=vec(0, 0.6, 0.6))
 
-# 開啟檔案 4-4_airdrag_vt_data.txt, 屬性為寫入, 先寫入欄位的標題
-file = open("4-4_airdrag_vt_data.txt", "w", encoding = "UTF-8")
-file.write("t(s), y(m), v(m/s), a(m/s^2)\n")
-tp = 0
+# b1 with air drag, b2 without air drag 
+b1 = sphere(pos=vec(2*size, h, 0), radius=size, color=c1, v=vec(0, 0, 0), a=vec(0, -g, 0))
+b2 = sphere(pos=vec(-2*size, h, 0), radius=size, color=c2, v=vec(0, 0, 0), a=vec(0, -g, 0))
+
+gd = graph(title="y-t plot", width=400, height=300, x=0, y=400, xtitle="t(s)", ytitle="y(m)")
+gd2 = graph(title="v-t plot", width=400, height=300, x=0, y=700, xtitle="t(s)", ytitle="v(m/s)")
+gd3 = graph(title="a-t plot", width=400, height=300, x=0, y=1000, xtitle="t(s)", ytitle="a(m/s<sup>2</sup>)")
+yt1 = gcurve(graph=gd, color=c1)
+yt2 = gcurve(graph=gd, color=c2)
+vt1 = gcurve(graph=gd2, color=c1)
+vt2 = gcurve(graph=gd2, color=c2)
+at1 = gcurve(graph=gd3, color=c1)
+at2 = gcurve(graph=gd3, color=c2)
+
+# 開啟檔案 4-4_airdrag_vt_data.csv, 屬性為寫入, 先寫入欄位的標題
+file = open("data.csv", "w", encoding="UTF-8")
+file.write("t(s), y1(m), y2(m), v1(m/s), v2(m/s), a1(m/s^2), a2(m/s^2)\n")
+tp=0
+
 # 設定計算終端速度用的變數
-eps = 0.00001
-v1 = 0
+eps=0.0000000001
+v1=0
 
 """
  3. 物體運動部分, 小球速度變化小於 eps 時停止運作
 """
 while(True):
 # 更新小球受力、加速度、速度、位置，畫 y-t 及 v-t 圖
-    f = -b*ball.v
-    ball.a = vector(0, -g, 0) + f/m
-    ball.v += ball.a*dt
-    ball.pos += ball.v*dt
-    yt.plot(pos = (t, ball.pos.y))
-    vt.plot(pos = (t, ball.v.y))
-    at.plot(pos = (t, ball.a.y))
+    f = -b*b1.v
+    b1.a = vec(0, -g, 0) + f/m
+    b1.v += b1.a*dt
+    b1.pos += b1.v*dt
+    b2.v += b2.a*dt
+    b2.pos += b2.v*dt
+    yt1.plot(pos=(t, b1.pos.y))
+    vt1.plot(pos=(t, b1.v.y))
+    at1.plot(pos=(t, b1.a.y))
+    yt2.plot(pos=(t, b2.pos.y))
+    vt2.plot(pos=(t, b2.v.y))
+    at2.plot(pos=(t, b2.a.y))
 # 每隔 0.1 秒將資料轉成字串後寫入檔案
     tc = t
     if(tc == 0 or tc - tp >= 0.1):
-        file.write(str(t) + "," + str(ball.pos.y) + "," + str(ball.v.y) + "," + str(ball.a.y) + "\n")
+        file.write(str(t) + "," + str(b1.pos.y) + "," + str(b2.pos.y) + "," + str(b1.v.y) + "," + str(b2.v.y) \
+                   + "," + str(b1.a.y) + "," + str(b2.a.y) + "\n")
         tp = tc
 # 計算終端速度, 當 abs(v2 - v1) > eps 時將 v2 的值指定給 v1, 當 abs(v2 - v1) > eps 時結束迴圈
-    v2 = ball.v.y
+    v2 = b1.v.y
     if(abs(v2 - v1) > eps): v1 = v2
     else: break
 # 更新時間
