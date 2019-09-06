e365c8d89935843e156c646d6e86988b0fd5f9c9
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-06 20:31:11 +0800

    Update 5-5_projectile_loop.py

--- 05.水平抛射/5-5_projectile_loop.py
+++ 05.水平抛射/5-5_projectile_loop.py
@@ -1,9 +1,12 @@
 """
  VPython教學: 5-5.水平抛射, 用for迴圈改變h, 記錄t、R
- 日期: 2018/3/21
+ Ver. 1: 2018/3/21
+ Ver. 2: 2018/6/13  加上用 matplotlib.pyplot 繪圖的部分
+ Ver. 3: 2019/9/6
  作者: 王一哲
 """
 from vpython import *
+import matplotlib.pyplot as plt
 
 """
  1. 參數設定, 設定變數及初始值
@@ -13,16 +16,16 @@ v0 = 5       # 小球水平初速
 L = 50       # 地板長度
 g = 9.8      # 重力加速度 9.8 m/s^2
 dt = 0.001   # 時間間隔
+data_h, data_x = [], []     # 儲存繪圖資料的串列
 
 """
  2. 畫面設定
 """
-scene = canvas(title = "Projectile", width = 800, height = 600, x = 0, y = 0, center = vector(0, 5, 0), \
-                          background = vector(0, 0.6, 0.6))
-floor = box(pos = vector(0, -size, 0), length = L, height = 0.01, width = 10, texture = textures.metal)
+scene = canvas(title="Projectile", width=800, height=600, x=0, y=0, center=vec(0, 5, 0), background=vec(0, 0.6, 0.6))
+floor = box(pos=vec(0, -size, 0), size=vec(L, 0.01, 10), texture=textures.metal)
 
-# 開啟檔案 data.txt, 屬性為寫入, 先寫入欄位的標題
-file = open("data.txt", "w", encoding = "UTF-8")
+# 開啟檔案 data.csv, 屬性為寫入, 先寫入欄位的標題
+file = open("data.csv", "w", encoding = "UTF-8")
 file.write("h(m), t(s), R(m)\n")
 
 """
@@ -30,16 +33,29 @@ file.write("h(m), t(s), R(m)\n")
 """
 for h in range(5, 51, 1):
     t = 0
-    ball = sphere(pos = vector(-L/2, h, 0), radius = size, texture = textures.wood, make_trail = True)
-    ball.v = vector(v0, 0, 0)
-    ball.a = vector(0, -g, 0)
-# 物體運動部分
-    while(ball.pos.y > floor.height/2):
+    ball = sphere(pos=vec(-L/2, h, 0), radius=size, texture=textures.wood, make_trail=True, v=vec(v0, 0, 0), a=vec(0, -g, 0))
+    # 物體運動部分
+    while(ball.pos.y - floor.pos.y > size + 0.5*floor.height):
         rate(500)
         ball.v += ball.a*dt
         ball.pos += ball.v*dt
         t += dt
     print(h, t, ball.pos.x + L/2)
     file.write(str(h) + "," + str(t) + "," + str(ball.pos.x + L/2) + "\n")
+    data_h.append(h)
+    data_x.append(ball.pos.x + L/2)
 
 file.close() # 關閉檔案
+
+# 用 matplotlib.pyplot 繪圖
+plt.figure(figsize=(8, 6), dpi=100)                   # 設定圖片尺寸
+plt.title('h - R plot', fontsize=18)                  # 設定圖標題
+plt.xlabel('h(m)', fontsize=16)                       # 設定坐標軸標籤
+plt.ylabel('R(m)', fontsize=16)
+plt.xticks(fontsize=12)                               # 設定坐標軸數字格式
+plt.yticks(fontsize=12)
+plt.grid(color='red', linestyle='--', linewidth=1)    # 設定格線顏色、種類、寬度
+plt.plot(data_h, data_x, marker='o', markerfacecolor='blue', markersize=8, color='skyblue', linewidth=3)   # 繪圖並設定線條顏色、寬度
+plt.savefig('h-R_plot.svg')                           # 儲存圖片
+plt.savefig('h-R_plot.png')
+plt.show()                                            # 顯示圖片
