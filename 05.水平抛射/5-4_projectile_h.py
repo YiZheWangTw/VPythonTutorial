d44459bea0d90be3f020c3a4fa793af32af75c7a
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-06 20:30:34 +0800

    Update 5-4_projectile_h.py

--- 05.水平抛射/5-4_projectile_h.py
+++ 05.水平抛射/5-4_projectile_h.py
@@ -1,6 +1,7 @@
 """
  VPython教學: 5-4.水平抛射, 改變h, 記錄R
- 日期: 2018/2/19
+ Ver. 1: 2018/2/19
+ Ver. 2: 2019/9/6
  作者: 王一哲
 """
 from vpython import *
@@ -18,22 +19,15 @@ dt = 0.001   # 時間間隔
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 box 物件產生地板 http://www.glowscript.org/docs/VPythonDocs/box.html
-    (3) 用 sphere 物件產生小球 http://www.glowscript.org/docs/VPythonDocs/sphere.html
-    (4) 用 graph 產生繪圖視窗 http://www.glowscript.org/docs/VPythonDocs/graph.html
 """
-scene = canvas(title = "Projectile", width = 800, height = 600, x = 0, y = 0, center = vector(0, h/2, 0), \
-                          background = vector(0, 0.6, 0.6))
-floor = box(pos = vector(0, -size, 0), length = L, height = 0.01, width = 10, texture = textures.metal)
-ball = sphere(pos = vector(-L/2, h, 0), radius = size, texture = textures.wood, make_trail = True)
-ball.v = vector(v0, 0, 0)
-ball.a = vector(0, -g, 0)
+scene = canvas(title="Projectile", width=800, height=600, x=0, y=0, center=vec(0, h/2, 0), background=vec(0, 0.6, 0.6))
+floor = box(pos=vec(0, -size, 0), size=vec(L, 0.01, 10), texture=textures.metal)
+ball = sphere(pos=vec(-L/2, h, 0), radius=size, texture=textures.wood, make_trail=True, v=vec(v0, 0, 0), a=vec(0, -g, 0))
 
 """
  3. 物體運動部分, 小球觸地時停止
 """
-while(ball.pos.y > floor.height/2):
+while(ball.pos.y - floor.pos.y > size + 0.5*floor.height):
     rate(1000)
     ball.v += ball.a*dt
     ball.pos += ball.v*dt
