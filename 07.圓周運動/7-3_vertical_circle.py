3b3e50d60ce6c94e2778894192beffbdcc6f0e4e
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-07 21:05:19 +0800

    Update 7-3_vertical_circle.py

--- 07.圓周運動/7-3_vertical_circle.py
+++ 07.圓周運動/7-3_vertical_circle.py
@@ -1,6 +1,7 @@
 """
  VPython教學: 7-3.鉛直面圓周運動
- 日期: 2018/3/30
+ Ver. 1: 2018/3/30
+ Ver. 2: 2019/9/7
  作者: 王一哲
 """
 from vpython import *
@@ -19,27 +20,20 @@ dt = 0.0001           # 時間間隔, 取 0.0001 以降低誤差
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 sphere 物件產生小球 http://www.glowscript.org/docs/VPythonDocs/sphere.html
-    (3) 用 cylinder 物件產生圓心及繩子 http://www.glowscript.org/docs/VPythonDocs/cylinder.html
-    (4) 用 arrow 物件產生表示速度及加速度的箭頭 http://www.glowscript.org/docs/VPythonDocs/arrow.html
-    (5) 用 graph 產生繪圖視窗 http://www.glowscript.org/docs/VPythonDocs/graph.html
 """
-scene = canvas(title = "Vertical Circle", width = 600, height = 600, x = 0, y = 0, background = color.black)
-ball = sphere(pos = vector(0, R, 0), radius = size, color = color.red, make_trail = True, retain = 100)
-ball.v = vector(-v0, 0, 0)
-center = cylinder(pos = vector(0, 0, -size), axis = vector(0, 0, 2*size), radius = 0.1*size, color = color.white)
-rope = cylinder(pos = vector(0, 0, 0), axis = ball.pos, radius = 0.1*size, color = color.yellow)
-arrow_v = arrow(pos = ball.pos, axis = vector(0, 0, 0), radius = 0.2*size, shaftwidth = 0.4*size, color = color.green)
-arrow_a = arrow(pos = ball.pos, axis = vector(0, 0, 0), radius = 0.2*size, shaftwidth = 0.4*size, color = color.blue)
-gd = graph(title = "plot", width = 600, height = 450, x = 0, y = 600, xtitle = "t(s)", ytitle = "green: v(m/s), red: at(m/s^2), blue: an(m/s^2)")
-vt_plot = gcurve(graph = gd, color = color.green)
-at_plot = gcurve(graph = gd, color = color.red)
-an_plot = gcurve(graph = gd, color = color.blue)
+scene = canvas(title="Vertical Circle", width=600, height=600, x=0, y=0, background=color.black)
+ball = sphere(pos=vec(0, R, 0), radius=size, color=color.red, make_trail=True, retain=100, v=vec(-v0, 0, 0))
+center = cylinder(pos=vec(0, 0, -size), axis=vec(0, 0, 2*size), radius=0.1*size, color=color.white)
+rope = cylinder(pos=vec(0, 0, 0), axis=ball.pos, radius=0.1*size, color=color.yellow)
+arrow_v = arrow(pos=ball.pos, axis=vec(0, 0, 0), radius=0.2*size, shaftwidth=0.4*size, color=color.green)
+arrow_a = arrow(pos=ball.pos, axis=vec(0, 0, 0), radius=0.2*size, shaftwidth=0.4*size, color=color.blue)
+gd = graph(title="plot", width=600, height=450, x=0, y=600, xtitle="<i>t</i>(s)",
+           ytitle="green: <i>v</i>(m/s), red: <i>a<sub>t</sub></i>(m/s<sup>2</sup>), blue: <i>a<sub>n</sub></i>(m/s<sup>2</sup>)")
+vt_plot = gcurve(graph=gd, color=color.green)
+at_plot = gcurve(graph=gd, color=color.red)
+an_plot = gcurve(graph=gd, color=color.blue)
 
 """
-
-
  3. 自訂函式, findan 計算法線加速度, findat 計算切線加速度
 """
 def findan(v, pos):
@@ -58,7 +52,7 @@ def findat(pos):
         atx = +absat*costheta
     elif((x <= 0 and y >= 0) or (x >= 0 and y <= 0)):
         atx = -absat*costheta
-    at = vector(atx, aty, 0)
+    at = vec(atx, aty, 0)
     return at
 
 """
