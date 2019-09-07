2faadad5afcd0a59ab662d80d8edf910b522fd28
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-07 21:04:09 +0800

    Update 7-1_circle.py

--- 07.圓周運動/7-1_circle.py
+++ 07.圓周運動/7-1_circle.py
@@ -1,6 +1,7 @@
 """
  VPython教學: 7-1.圓周運動
- 日期: 2018/2/22
+ Ver. 1: 2018/2/22
+ Ver. 2: 2019/9/7
  作者: 王一哲
 """
 from vpython import *
@@ -17,26 +18,21 @@ dt = 0.001            # 時間間隔
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 box 物件產生地板 http://www.glowscript.org/docs/VPythonDocs/box.html
-    (3) 用 sphere 物件產生小球 http://www.glowscript.org/docs/VPythonDocs/sphere.html
-    (4) 用 arrow 物件產生表示速度及加速度的箭頭 http://www.glowscript.org/docs/VPythonDocs/arrow.html
 """
-scene = canvas(title = "Circle", width = 800, height = 400, x = 0, y = 0, background = vector(0, 0.6, 0.6))
-scene.camera.pos = vector(0, L/2, L/2)
-scene.camera.axis = vector(0, -L/2, -L/2)
-floor = box(pos = vector(0, -size, 0), length = L, height = 0.01, width = L, texture = textures.metal)
-ball = sphere(pos = vector(R, size, 0), radius = size, color = color.red, make_trail = True, retain = 100)
-ball.v = vector(0, 0, -v0)
-arrow_v = arrow(pos = ball.pos, axis = ball.v, radius = 0.2*size, shaftwidth = 0.4*size, color = color.green)
-arrow_a = arrow(pos = ball.pos, axis = vector(0, 0, 0), radius = 0.2*size, shaftwidth = 0.4*size, color = color.blue)
+scene = canvas(title="Circle", width=800, height=400, x=0, y=0, background=vec(0, 0.6, 0.6))
+scene.camera.pos = vec(0, L/2, L/2)
+scene.camera.axis = vec(0, -L/2, -L/2)
+floor = box(pos=vec(0, -size, 0), size=vec(L, 0.01, L), texture=textures.metal)
+ball = sphere(pos=vec(R, size, 0), radius=size, color=color.red, make_trail=True, retain=100, v=vec(0, 0, -v0))
+arrow_v = arrow(pos=ball.pos, axis=ball.v, radius=0.2*size, shaftwidth=0.4*size, color=color.green)
+arrow_a = arrow(pos=ball.pos, axis=vec(0, 0, 0), radius=0.2*size, shaftwidth=0.4*size, color=color.blue)
 
 """
  3. 物體運動部分
 """
 while(True):
     rate(1000)
-    axis = vector(0, size, 0) - ball.pos
+    axis = vec(0, size, 0) - ball.pos
     ball.a = (ball.v.mag2 / R) * axis.norm()
     ball.v += ball.a*dt
     ball.pos += ball.v*dt
