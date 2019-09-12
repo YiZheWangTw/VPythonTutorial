97dac496b01099fd9bab69600f4e86627beebfe2
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-12 20:17:12 +0800

    Update 12-1_Kepler_1st.py

--- 12.行星運動/12-1_Kepler_1st.py
+++ 12.行星運動/12-1_Kepler_1st.py
@@ -2,6 +2,7 @@
  VPython教學: 12-1.行星運動, 自訂星球速度、距離, 可更改萬有引力定律中 r 的次方
  Ver. 1: 2018/2/25
  Ver. 2: 2018/3/1
+ Ver. 3: 2019/9/8
  作者: 王一哲
 """
 from vpython import *
@@ -21,29 +22,25 @@ dt = 60*60             # 時間間隔
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 sphere 物件產生星球 http://www.glowscript.org/docs/VPythonDocs/sphere.html
-    (3) 用 arrow 物件產生表示速度、加速度用的箭頭 http://www.glowscript.org/docs/VPythonDocs/arrow.html
-    (4) 用 graph 產生繪圖視窗 http://www.glowscript.org/docs/VPythonDocs/graph.html
 """
-scene = canvas(title = "Planetary Motion", width = 600, height = 600, x = 0, y = 0, background = color.black)
+scene = canvas(title="Planetary Motion", width=600, height=600, x=0, y=0, background=color.black)
 # 產生太陽 sun, 行星 planet
-sun = sphere(pos = vector(0,0,0), radius = size, m = sun_m, color = color.orange, emissive = True)
-planet = sphere(pos = vector(d, 0, 0), radius = size, texture = textures.earth, make_trail = True, trail_color = color.blue, trail_radius = 0.1*size)
-planet.v = vector(v0*cos(theta), v0*sin(theta), 0)
-# 原來的寫法為 scene.lights = [local_light(pos = vector(0,0,0), color = color.white)]
+sun = sphere(pos=vec(0,0,0), radius=size, m=sun_m, color=color.orange, emissive=True)
+planet = sphere(pos=vec(d, 0, 0), radius=size, texture=textures.earth, make_trail=True,
+                trail_color=color.blue, trail_radius=0.1*size, v=vec(v0*cos(theta), v0*sin(theta), 0))
+# 原來的寫法為 scene.lights=[local_light(pos=vec(0,0,0), color=color.white)]
 # 在 VPython 7 中 canvas.lights 無法設定為 local_light, 只能另外在太陽處放置另一個光源 lamp
-lamp = local_light(pos = vector(0,0,0), color = color.white)
+lamp = local_light(pos=vec(0,0,0), color=color.white)
 # 產生表示速度、加速度用的箭頭
-arrow_v = arrow(pos = planet.pos, axis = vector(0, 0, 0), shaftwidth = 0.3*size, color = color.green)
-arrow_a = arrow(pos = planet.pos, axis = vector(0, 0, 0), shaftwidth = 0.3*size, color = color.magenta)
+arrow_v = arrow(pos=planet.pos, axis=vec(0, 0, 0), shaftwidth=0.3*size, color=color.green)
+arrow_a = arrow(pos=planet.pos, axis=vec(0, 0, 0), shaftwidth=0.3*size, color=color.magenta)
 # 產生繪圖視窗
-gdr = graph(title = "r-t plot", width = 600, height = 450, x = 0, y = 600, xtitle = "t(s)", ytitle = "r(m)")
-gdv = graph(title = "v-t plot", width = 600, height = 450, x = 0, y = 1050, xtitle = "t(s)", ytitle = "v(m/s)")
-gda = graph(title = "a-t plot", width = 600, height = 450, x = 0, y = 1500, xtitle = "t(s)", ytitle = "a(m/s^2)")
-rt = gcurve(graph = gdr, color = color.blue)
-vt = gcurve(graph = gdv, color = color.green)
-at = gcurve(graph = gda, color = color.magenta)
+gdr = graph(title="r-t plot", width=600, height=450, x=0, y=600, xtitle="<i>t</i> (s)", ytitle="<i>r</i> (m)")
+gdv = graph(title="v-t plot", width=600, height=450, x=0, y=1050, xtitle="<i>t</i> (s)", ytitle="<i>v</i> (m/s)")
+gda = graph(title="a-t plot", width=600, height=450, x=0, y=1500, xtitle="<i>t</i> (s)", ytitle="<i>a</i> (m/s<sup>2</sup>")
+rt = gcurve(graph=gdr, color=color.blue)
+vt = gcurve(graph=gdv, color=color.green)
+at = gcurve(graph=gda, color=color.magenta)
 
 """
  3. 星球運動部分
@@ -51,7 +48,7 @@ at = gcurve(graph = gda, color = color.magenta)
 while(True):
     rate(60*24*2)
 # 更新行星加速度、速度、位置
-    planet.a = - G*sun.m / planet.pos.mag**n * planet.pos.norm()
+    planet.a = -G*sun.m / planet.pos.mag**n * planet.pos.norm()
     planet.v += planet.a*dt
     planet.pos += planet.v*dt
 # 更新代表速度、加速度的箭頭位置、方向、長度
@@ -60,8 +57,8 @@ while(True):
     arrow_v.axis = planet.v*2E6    # 乘以 2E6 放大箭頭, 否則會看不見
     arrow_a.axis = planet.a*1E13   # 乘以 1E13 放大箭頭, 否則會看不見
 # 畫出 r-t, v-t, a-t 圖
-    rt.plot(pos = (t, planet.pos.mag))
-    vt.plot(pos = (t, planet.v.mag))
-    at.plot(pos = (t, planet.a.mag))
+    rt.plot(pos=(t, planet.pos.mag))
+    vt.plot(pos=(t, planet.v.mag))
+    at.plot(pos=(t, planet.a.mag))
 # 更新時間
     t += dt
