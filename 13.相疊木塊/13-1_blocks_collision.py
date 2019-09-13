e941c5d70d9f9cfd309d3b9f3fb37c4069f1deef
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-13 08:57:48 +0800

    Update 13-1_blocks_collision.py

--- 13.相疊木塊/13-1_blocks_collision.py
+++ 13.相疊木塊/13-1_blocks_collision.py
@@ -1,8 +1,9 @@
-﻿"""
+"""
  VPython教學: 13-1.相疊的木塊完全非彈性碰撞
  Ver. 1: 2017/6/16
  Ver. 2: 2018/1/4   修改為 Python 3.X 版程式碼
  Ver. 3: 2018/2/26   增加 v-t 圖
+ Ver. 4: 2019/9/8
  作者: 王一哲
 """
 
@@ -29,30 +30,27 @@ i, te = 0, -1                           # 記錄 b1、b2 達到等速度經過�
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 box 物件產生木塊及地板 http://vpython.org/contents/docs/box.html
-    (3) 用 graph 物件產生繪圖視窗 http://www.glowscript.org/docs/VPythonDocs/graph.html
 """
 # 產生動畫視窗
-scene = canvas(title = "Two Blocks", width = 800, height = 300, center = vector(0, 0.4, 0), background = vector(0, 0.6, 0.6))
+scene = canvas(title="Two Blocks", width=800, height=300, center=vec(0, 0.4, 0), background=vec(0, 0.6, 0.6))
 # 產生地板
-floor = box(length = (xmax - xmin), height = 0.05, width = 0.8, pos = vector(0, -h2/2.0, 0), color = color.blue)
+floor = box(pos=vec(0, -0.5*h2, 0), size=vec(xmax - xmin, 0.05, 0.8), color=color.blue)
 # 產生下方木塊 b1, 位於畫面最左側 xmin + d1/2, 初速度 v1
-b1 = box(length = d1, height = h1, width = w1, color = c1, pos = vector(xmin + d1/2, 0, 0))
-b1.v = vector(v1, 0, 0)
+b1 = box(pos=vec(xmin + d1/2, 0, 0), size=vec(d1, h1, w1), color = c1, v=vec(v1, 0, 0))
 # 產生上方木塊 b2, 若 v2 >= v1 則位於畫面最左側 xmin + d2/2; 若 v2 < v1, 則位於 b1 最右側 xmin + d1 - d2/2, 初速度 v2
-if(v2 >= v1): bx = xmin + d2/2
-else: bx = xmin + d1 - d2/2
-b2 = box(length = d2, height = h2, width = w2, color = c2, pos = vector(bx, h1, 0))
-b2.v = vector(v2, 0, 0)
+if(v2 >= v1): bx = xmin + 0.5*d2
+else: bx = xmin + d1 - 0.5*d2
+b2 = box(pos=vec(bx, h1, 0), size=vec(d2, h2, w2), color=c2, v=vec(v2, 0, 0))
 # 繪圖部分
-gd = graph(title = "E - t plot", x = 0, y = 300, width = 600, height = 450, xtitle = "t (s)", ytitle = "red: K1, green: K2, blue: E(J)")
-kt1 = gcurve(graph = gd, color = c1)
-kt2 = gcurve(graph = gd, color = c2)
-et = gcurve(graph = gd, color = color.blue)
-gd2 = graph(title = "v - t plot", x = 0, y = 750, width = 600, height = 450, xtitle = "t (s)", ytitle = "red: v1, green: v2(m/s)")
-vt1 = gcurve(graph = gd2, color = c1)
-vt2 = gcurve(graph = gd2, color = c2)
+gd = graph(title="E - t plot", x=0, y=300, width=600, height=450, xtitle="<i>t</i> (s)",
+           ytitle="red: <i>K</i><sub>1</sub>, green: <i>K</i><sub>2</sub>, blue: <i>E</i> (J)")
+kt1 = gcurve(graph=gd, color=c1)
+kt2 = gcurve(graph=gd, color=c2)
+et = gcurve(graph=gd, color=color.blue)
+gd2 = graph(title="v - t plot", x=0, y=750, width=600, height=450, xtitle="<i>t</i> (s)",
+            ytitle="red: <i>v</i><sub>1</sub>, green: <i>v</i><sub>2</sub> (m/s)")
+vt1 = gcurve(graph=gd2, color=c1)
+vt2 = gcurve(graph=gd2, color=c2)
 
 """
  3. 物體運動部分, 重複執行直到: (1) b1 抵達邊緣時, (2) b2 抵達 b1 邊緣時
@@ -63,16 +61,16 @@ while((b1.pos.x <= xmax - d1/2) and (b2.pos.x + d2/2 <= b1.pos.x + d1/2 + 0.001)
 #由 b1, b2 速度判斷動摩擦力量值及加速度方向, 
     if(b2.v.x > b1.v.x):
         force = mu * m2 * g
-        b1.a = vector(force / m1, 0, 0)
-        b2.a = vector(-force / m2, 0, 0)
+        b1.a = vec(force / m1, 0, 0)
+        b2.a = vec(-force / m2, 0, 0)
     elif(b2.v.x < b1.v.x):
         force = mu * m2 * g
-        b1.a = vector(-force / m1, 0, 0)
-        b2.a = vector(force / m2, 0, 0)
+        b1.a = vec(-force / m1, 0, 0)
+        b2.a = vec(force / m2, 0, 0)
     else:
         force = 0
-        b1.a = vector(0, 0, 0)
-        b2.a = vector(0, 0, 0)
+        b1.a = vec(0, 0, 0)
+        b2.a = vec(0, 0, 0)
 # 記錄 b1、b2 達到等速度需要的時間
     if(abs(b2.v.x - b1.v.x) < 0.0005 and i == 0):
         te = t
