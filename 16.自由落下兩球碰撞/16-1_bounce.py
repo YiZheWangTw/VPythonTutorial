da389f961e36db1c12355ded08571fa21f16b69a
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-14 11:36:03 +0800

    Update 16-1_bounce.py

--- 16.自由落下兩球碰撞/16-1_bounce.py
+++ 16.自由落下兩球碰撞/16-1_bounce.py
@@ -1,6 +1,7 @@
 """
  VPython教學: 16-1.自由落下兩球碰撞
- 日期: 2018/2/28
+ Ver. 1: 2018/2/28
+ Ver. 2: 2019/9/14
  作者: 王一哲
 """
 from vpython import *
@@ -12,37 +13,31 @@ from vpython import *
 """
 r1, m1, c1 = 2, 2, color.red     # 小球1半徑, 質量, 顏色
 r2, m2, c2 = 1, 1, color.green   # 小球2半徑, 質量, 顏色
-h = 15            # 小球離地高度
-L = 40            # 地板邊長
+h, L = 15, 40     # 小球離地高度, 地板邊長
 g = 9.8           # 重力加速度 9.8 m/s^2
-t = 0             # 時間
-dt = 0.001        # 時間間隔
+t, dt = 0, 0.001  # 時間, 時間間隔
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 box 物件產生地板 http://www.glowscript.org/docs/VPythonDocs/box.html
-    (3) 用 sphere 物件產生小球 http://www.glowscript.org/docs/VPythonDocs/sphere.html
-    (4) 用 graph 產生繪圖視窗 http://www.glowscript.org/docs/VPythonDocs/graph.html
 """
 # 產生動畫視窗
-scene = canvas(title = "Free Fall and Collision", width = 600, height = 600, x = 0, y = 0, center = vector(0, h/2, 0), \
-                          background = vector(0, 0.6, 0.6), range = L)
+scene = canvas(title="Free Fall and Collision", width=600, height=600, x=0, y=0,
+               center=vec(0, h/2, 0), background=vec(0, 0.6, 0.6), range=L)
 # 產生地板
-floor = box(pos = vector(0, -r1, 0), length = L, height = 0.01, width = L, texture = textures.wood)
+floor = box(pos=vec(0, -r1, 0), size=vec(L, 0.01, L), texture=textures.wood)
 # 產生小球並設定初速度、加速度
-b1 = sphere(pos = vector(0, h, 0), radius = r1, color = c1)
-b1.v, b1.a = vector(0, 0, 0), vector(0, -g, 0)
-b2 = sphere(pos = vector(0, h + r1 + r2, 0), radius = r2, color = c2)
-b2.v, b2.a = vector(0, 0, 0), vector(0, -g, 0)
+b1 = sphere(pos=vec(0, h, 0), radius=r1, color=c1, m=m1, v=vec(0, 0, 0), a=vec(0, -g, 0))
+b2 = sphere(pos=vec(0, h+r1+r2, 0), radius=r2, color=c2, m=m2, v=vec(0, 0, 0), a=vec(0, -g, 0))
 # y-t plot
-gd = graph(title = "y-t plot", width = 600, height = 450, x = 0, y = 600, xtitle = "t(s)", ytitle = "red: y1, green:y2(m)")
-yt1 = gcurve(graph = gd, color = c1)
-yt2 = gcurve(graph = gd, color = c2)
+gd = graph(title="<i>y</i>-<i>t</i> plot", width=600, height=450, x=0, y=600,
+           xtitle="<i>t</i> (s)", ytitle="red: <i>y</i><sub>1</sub>, green: <i>y</i><sub>2</sub> (m)")
+yt1 = gcurve(graph=gd, color=c1)
+yt2 = gcurve(graph=gd, color=c2)
 # v-t plot
-gd2 = graph(title = "v-t plot", width = 600, height = 450, x = 0, y = 1050, xtitle = "t(s)", ytitle = "red: v1, green:v2(m/s)")
-vt1 = gcurve(graph = gd2, color = c1)
-vt2 = gcurve(graph = gd2, color = c2)
+gd2 = graph(title="<i>v</i>-<i>t</i> plot", width=600, height=450, x=0, y=1050,
+            xtitle="<i>t</i> (s)", ytitle="red: <i>v</i><sub>2</sub>, green: <i>v</i><sub>2</sub> (m/s)")
+vt1 = gcurve(graph=gd2, color=c1)
+vt2 = gcurve(graph=gd2, color=c2)
 # 一維彈性碰撞速度公式
 def af_col_v(m1, m2, v1, v2):
     v1_prime = (m1-m2)/(m1+m2)*v1 + (2*m2)/(m1+m2)*v2
@@ -60,14 +55,14 @@ while(t < 20):
     b2.v += b2.a*dt
     b2.pos += b2.v*dt
 # 繪製小球 y-t、v-t 圖
-    yt1.plot(pos = (t, b1.pos.y))
-    yt2.plot(pos = (t, b2.pos.y))
-    vt1.plot(pos = (t, b1.v.y))
-    vt2.plot(pos = (t, b2.v.y))
+    yt1.plot(pos=(t, b1.pos.y))
+    yt2.plot(pos=(t, b2.pos.y))
+    vt1.plot(pos=(t, b1.v.y))
+    vt2.plot(pos=(t, b2.v.y))
 # 若 b1 撞到地板則反彈
     if(b1.pos.y <= 0 and b1.v.y < 0): b1.v.y = -b1.v.y
 # 若 b1、b2 相撞則計算撞後速度並重新指定給 vy1, vy2
     if(b2.pos.y - b1.pos.y <= r1 + r2):
-        b1.v.y, b2.v.y = af_col_v(m1, m2, b1.v.y, b2.v.y)
+        b1.v.y, b2.v.y = af_col_v(b1.m, b2.m, b1.v.y, b2.v.y)
 # 更新時間
     t += dt
