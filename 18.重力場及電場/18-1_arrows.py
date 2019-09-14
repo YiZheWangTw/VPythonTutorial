1099039d11d22d66b436aaa7917194ca0b89a7fe
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-14 22:09:23 +0800

    Update 18-1_arrows.py

--- 18.重力場及電場/18-1_arrows.py
+++ 18.重力場及電場/18-1_arrows.py
@@ -7,10 +7,11 @@ from vpython import *
 
 N, L = 4, 20
 
-scene = canvas(title = "Arrows", width = 600, height = 600, x = 0, y = 0, background = color.black)
+scene = canvas(title="Arrows", width=600, height=600, x=0, y=0, background=color.black)
 
 fields = []
 for i in range(N):
     for j in range(N):
         for k in range(N):
-            fields.append(arrow(pos = vector(L/N*i - L/2, L/N*j - L/2, L/N*k - L/2), axis = vector(4, 0, 0), radius = 1, color = color.green))
+            fields.append(arrow(pos=vec(L/N*i - L/2, L/N*j - L/2, L/N*k - L/2),
+                                axis=vector(4, 0, 0), radius=1, color=color.green))
