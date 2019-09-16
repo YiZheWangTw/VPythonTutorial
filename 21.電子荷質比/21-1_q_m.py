c26502836a8f844c1abd85ce2056711e1c4ced97
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-16 21:32:52 +0800

    Update 21-1_q_m.py

--- 21.電子荷質比/21-1_q_m.py
+++ 21.電子荷質比/21-1_q_m.py
@@ -1,6 +1,7 @@
 """
  VPython教學: 21-1.電子荷質比
- 日期: 2018/4/8
+ Ver. 1: 2018/4/8
+ Ver. 2: 2019//9/16
  作者: 王一哲
 """
 from vpython import *
@@ -14,40 +15,28 @@ from vpython import *
     (5) 向下但不會撞到平行帶電板 v0 = 10, q = 2E-9, V = 1, d = 0.1
     (6) 向下但不會撞到平行帶電板 v0 = 20, q = 3E-9, V = 1, d = 0.1
 """
-size = 0.005                  # 粒子半徑
-m = 1E-11                     # 粒子質量
-v0 = 10                       # 粒子水平速度
-q = -3E-9                     # 粒子電量
-V = 1                         # 平行帶電板間的電壓
-d = 0.1                       # 平行帶電板間的距離
-L = 0.2                       # 平行帶電板間的長度
-E_field = vector(0, -V/d, 0)  # 電場
-t = 0                         # 時間
-dt = 1E-5                     # 時間間隔
+size, m, q, v0 = 0.005, 1E-11, -3E-9, 10   # 粒子半徑, 質量, 電量, 水平速度
+V, d, L = 1, 0.1, 0.2      # 平行帶電板間的電壓, 距離, 長度
+E_field = vec(0, -V/d, 0)  # 電場
+t, dt = 0, 1E-5            # 時間, 時間間隔
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 box 物件產生平行帶電板 http://www.glowscript.org/docs/VPythonDocs/box.html
-    (3) 用 cylinder 物件產生水平線 http://www.glowscript.org/docs/VPythonDocs/cylinder.html
-    (4) 用 sphere 物件產生帶電粒子 http://www.glowscript.org/docs/VPythonDocs/sphere.html
-    (5) 用 arrow 物件產生表示速度、加速度用的箭頭 http://www.glowscript.org/docs/VPythonDocs/arrow.html
-    (6) 用 label 產生標籤 http://www.glowscript.org/docs/VPythonDocs/label.html
 """
 # 產生動畫視窗、平行帶電板、水平線、帶電粒子
-scene = canvas(title = "q/m", width = 800, height = 600, x = 0, y = 0, center = vector(0, 0, 0), \
-                          background = color.black)
-p1 = box(pos = vector(-L/2, d/2, 0), length = L, height = 0.01*L, width = L, color = color.blue)
-p2 = box(pos = vector(-L/2, -d/2, 0), length = L, height = 0.01*L, width = L, color = color.blue)
-screen = box(pos = vector(L, 0, 0), length = 0.01*L, height = 1.5*L, width = L, color = color.blue)
-line = cylinder(pos = vector(-1.5*L, 0, 0), radius = 0.2*size, axis = vector(3*L, 0, 0), color = color.yellow)
-charge = sphere(pos = vector(-1.5*L, 0, 0), v = vector(v0, 0, 0), radius = size, color = color.red, make_trail = True)
+scene = canvas(title="Mass-to-Charge Ratio", width=800, height=600, x=0, y=0,
+               center=vec(0, 0, 0), background = color.black)
+p1 = box(pos=vec(-L/2, d/2, 0), size=vec(L, 0.01*L, L), color=color.blue)
+p2 = box(pos=vec(-L/2, -d/2, 0), size=vec(L, 0.01*L, L), color=color.blue)
+screen = box(pos=vec(L, 0, 0), size=vec(0.01*L, 1.5*L, L), color=color.blue)
+line = cylinder(pos=vec(-1.5*L, 0, 0), radius=0.2*size, axis=vec(3*L, 0, 0), color=color.yellow)
+charge = sphere(pos=vec(-1.5*L, 0, 0), v=vec(v0, 0, 0), radius=size, color=color.red, m=m, make_trail=True)
 # 產生表示電場的箭頭及標籤
-arrow_E = arrow(pos = vector(-L, d/2, 0), axis = vector(0, -0.1, 0), shaftwidth = size, color = color.green)
-label_E = label(pos = vector(-L, d/2, 0), text = "E", xoffset = -25, yoffset = 25, color = color.green, font = "sans")
+arrow_E = arrow(pos=vec(-L, d/2, 0), axis=vec(0, -0.1, 0), shaftwidth=size, color=color.green)
+label_E = label(pos=vec(-L, d/2, 0), text="E", xoffset=-25, yoffset=25, color=color.green, font="sans")
 # 產生表示速度、加速度的箭頭
-arrow_v = arrow(pos = charge.pos, shaftwidth = 0.3*size, color = color.cyan)
-arrow_a = arrow(pos = charge.pos, shaftwidth = 0.3*size, color = color.magenta)
+arrow_v = arrow(pos=charge.pos, shaftwidth=0.3*size, color=color.cyan)
+arrow_a = arrow(pos=charge.pos, shaftwidth=0.3*size, color=color.magenta)
 
 """
  3. 物體運動部分, 當帶電粒子到達屏幕或撞到平行帶電板時停止
@@ -58,9 +47,9 @@ while(0 < charge.pos.x < screen.pos.x - screen.length/2 - size or \
     rate(500)
 # 計算帶電粒子所受合力, 在平行帶電板間才有電場
     if(-L <= charge.pos.x <= 0): F = q*E_field
-    else: F = vector(0, 0, 0)
+    else: F = vec(0, 0, 0)
 # 更新帶電粒子加速度、速度、位置
-    charge.a = F/m
+    charge.a = F/charge.m
     charge.v += charge.a*dt
     charge.pos += charge.v*dt
 # 更新表示速度、加速度的箭頭, 只畫出方向以避免動畫自動縮小
@@ -71,7 +60,8 @@ while(0 < charge.pos.x < screen.pos.x - screen.length/2 - size or \
 # 當帶電粒子離開平行帶電板時畫出水平線標示 y 方向位移
     xc = charge.pos.x
     if(xp < 0 and xc > 0):
-        line2 = cylinder(pos = vector(-1.5*L, charge.pos.y, 0), radius = 0.2*size, axis = vector(3*L, 0, 0), color = color.yellow)
+        line2 = cylinder(pos=vec(-1.5*L, charge.pos.y, 0), radius=0.2*size,
+                         axis=vec(3*L, 0, 0), color=color.yellow)
     xp = xc
 # 更新時間
     t += dt
