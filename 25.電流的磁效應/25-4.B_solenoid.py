f3eafae924d2bd9dd768921465befb8aed6fbd65
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-09-19 17:42:17 +0800

    Update 25-4.B_solenoid.py

--- 25.電流的磁效應/25-4.B_solenoid.py
+++ 25.電流的磁效應/25-4.B_solenoid.py
@@ -1,6 +1,7 @@
 """
  VPython教學: 25-4.載流螺線管產生的磁場
- 日期: 2018/4/21
+ Ver. 1: 2018/4/21
+ Ver. 2: 2019/9/19
  作者: 王一哲
 """
 from vpython import *
@@ -23,19 +24,13 @@ Bmax = 5              # 顯示的磁場最大值
 
 """
  2. 畫面設定
-    (1) 用 canvas 物件作為顯示動畫用的視窗 http://www.glowscript.org/docs/VPythonDocs/canvas.html
-    (2) 用 sphere 物件產生小球 http://www.glowscript.org/docs/VPythonDocs/sphere.html
-    (3) 用 cylinder 物件產生一小段導線 http://www.glowscript.org/docs/VPythonDocs/cylinder.html
-    (4) 用 arrow 物件產生表示磁場用的箭頭 http://www.glowscript.org/docs/VPythonDocs/arrow.html
 """
 # 產生動畫視窗
-scene = canvas(title = "Magnetic Field of Current Solenoid", width = 600, height = 600, x = 0, y = 0, \
-               center = vector(0, L/10, 0), background = color.black)
+scene = canvas(title="Magnetic Field of Current Solenoid", width=600, height=600, x=0, y=0, 
+               center=vec(0, 0.1*L, 0), background=color.black)
 
 # 產生空白串列 points, 在螺線管上等距離取點並填入 points 中
-points = []
-for i in range(n):
-    points.append(sphere(pos = vector(L/2 - i*L/n, r*cos(2*pi/n*num*i), r*sin(2*pi/n*num*i)), radius = point_size, color = color.cyan))
+points = [sphere(pos=vec(L/2 - i*L/n, r*cos(2*pi/n*num*i), r*sin(2*pi/n*num*i)), radius=point_size, color=color.cyan) for i in range(n)]
 
 # 產生空白串列 segs, 從 points 依序一次讀取兩個點, 計算軸向量, 中點位置, 將螺線管切成很多小圓柱並填入 segs 中
 segs = []
@@ -50,21 +45,19 @@ locations = []
 for i in range(N+1):
     for j in range(N+1):
         for k in range(N+1):
-            location = vector(L/N*i - L/2, L/N*j - L/2, L/N*k - L/2)
+            location = vec(L/N*i - L/2, L/N*j - L/2, L/N*k - L/2)
             locations.append(location)
 
 # 自訂函式 magnetic, 計算某個位置的磁場
 def magnetic(loc, segments):
-    field = vector(0, 0, 0)
+    field = vec(0, 0, 0)
     for segment in segments:
         axis = loc - segment.pos
         field += mu*current/(4*pi)*segment.axis.mag*cross(segment.axis, axis.norm())/axis.mag2
     return field
 
 # 依序讀取串列 locations 的元素, 在對應的位置產生箭頭
-fields = []
-for location in locations:
-    fields.append(arrow(pos = location, axis = vector(0, 0, 0), color = color.green))
+fields = [arrow(pos=location, axis=vec(0, 0, 0), color=color.green) for location in locations]
 
 # 更新箭頭的長度及方向, 若磁場量值 >= Bmax 則設定為 Bmax, 以避免箭頭蓋住其它東西
 # 量值接近 Bmax 偏紅色, 量值接近 0 偏綠色
@@ -72,4 +65,4 @@ for field in fields:
     value = magnetic(field.pos, segs)
     if(value.mag >= Bmax): value = value/value.mag * Bmax
     field.axis = value
-    field.color = vector(value.mag/Bmax, 1 - value.mag/Bmax, 0)
+    field.color = vec(value.mag/Bmax, 1 - value.mag/Bmax, 0)
