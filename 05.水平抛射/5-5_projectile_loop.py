dec39eb5835cae77568a0a73fe87021d38e71c02
Yi-Zhe Wang <39249857+YiZheWangTw@users.noreply.github.com>
2019-10-30 22:57:04 +0800

    Update 5-5_projectile_loop.py

--- 05.水平抛射/5-5_projectile_loop.py
+++ 05.水平抛射/5-5_projectile_loop.py
@@ -2,7 +2,7 @@
  VPython教學: 5-5.水平抛射, 用for迴圈改變h, 記錄t、R
  Ver. 1: 2018/3/21
  Ver. 2: 2018/6/13  加上用 matplotlib.pyplot 繪圖的部分
- Ver. 3: 2019/10/10 改用 with 開啟檔案
+ Ver. 3: 2019/10/10 改用 with 開啟檔案, 上課用的版本
  作者: 王一哲
 """
 from vpython import *
@@ -16,7 +16,7 @@ v0 = 5       # 小球水平初速
 L = 50       # 地板長度
 g = 9.8      # 重力加速度 9.8 m/s^2
 dt = 0.001   # 時間間隔
-data_h, data_x = [], []     # 儲存繪圖資料的串列
+data_y, data_x = [], []     # 儲存繪圖資料的串列
 
 """
  2. 畫面設定
@@ -24,8 +24,8 @@ data_h, data_x = [], []     # 儲存繪圖資料的串列
 scene = canvas(title="Projectile", width=800, height=600, x=0, y=0, center=vec(0, 5, 0), background=vec(0, 0.6, 0.6))
 floor = box(pos=vec(0, -size, 0), size=vec(L, 0.01, 10), texture=textures.metal)
 
-# 開啟檔案 data.csv, 屬性為寫入, 先寫入欄位的標題
-with open("5-5_data.csv", "w", encoding = "UTF-8") as file:
+# 開啟檔案 5-5_data.csv, 屬性為寫入, 先寫入欄位的標題
+with open("5-5_data.csv", "w", encoding="UTF-8") as file:
     file.write("h(m), t(s), R(m)\n")
 
 """
@@ -46,19 +46,19 @@ def main(h):
 """
 for h in range(5, 51, 1):
     t, r = main(h)
-    with open("5-5_data.csv", "a", encoding = "UTF-8") as file:
+    with open("5-5_data.csv", "a", encoding="UTF-8") as file:
         file.write(str(h) + "," + str(t) + "," + str(r) + "\n")
-    data_h.append(h)
-    data_x.append(r)
+    data_y.append(t)
+    data_x.append(h)
 
 # 用 matplotlib.pyplot 繪圖
 plt.figure(figsize=(8, 6), dpi=100)                   # 設定圖片尺寸
 plt.xlabel(r'$h ~\mathrm{(m)}$', fontsize=16)         # 設定坐標軸標籤
-plt.ylabel(r'$R ~\mathrm{(m)}$', fontsize=16)
+plt.ylabel(r'$t ~\mathrm{(s)}$', fontsize=16)
 plt.xticks(fontsize=12)                               # 設定坐標軸數字格式
 plt.yticks(fontsize=12)
 plt.grid(color='red', linestyle='--', linewidth=1)    # 設定格線顏色、種類、寬度
-plt.plot(data_h, data_x, marker='o', markerfacecolor='blue', markersize=8, color='skyblue', linewidth=3)   # 繪圖並設定線條顏色、寬度
-plt.savefig('h-R_plot.svg')                           # 儲存圖片
-plt.savefig('h-R_plot.png')
+plt.plot(data_x, data_y, marker='o', markerfacecolor='blue', markersize=8, color='skyblue', linewidth=3)   # 繪圖並設定線條顏色、寬度
+plt.savefig('t-h_plot.svg')                           # 儲存圖片
+plt.savefig('t-h_plot.png')
 plt.show()                                            # 顯示圖片
